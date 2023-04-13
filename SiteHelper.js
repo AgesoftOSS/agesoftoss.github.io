71a5a02a14b511204c0da56f512b0e4b434fe60b
Copy <71971770+Copy05@users.noreply.github.com>
2023-04-13 12:56:28 +0200

    Site Upload

---
+++ SiteHelper.js
@@ -0,0 +1,35 @@
+const Footer = document.getElementById("footer")
+const Header = document.getElementById("header")
+
+const HeaderHTML = `
+<a href="index.html" class="logohref">
+            <p class="logo">Agesoft</p>
+</a>
+        <ul class="nav_links">
+            <li><a href="about.html">About</a></li>
+            <li><a href="projects.html">Solutions</a></li>
+            <li><a href="projects.html">Projects</a></li>
+        </ul>
+`
+
+const FooterHTML = `
+<div class="footsections">
+            <div class="footsec">
+                <p class="feetsec">About Agesoft:</p>
+                <a href="https://entertainmasters.github.io/carriers.html" class="footlink">Carriers</a><br>
+            </div>
+
+            <div class="footsec">
+                <p class="feetsec">Legal:</p>
+                <a href="https://entertainmasters.github.io/privacy.html" class="footlink">Privacy</a><br>
+                <a href="https://entertainmasters.github.io/impress.html" class="footlink">Inpress</a>
+            </div>
+        </div>
+
+        <p class="copyright">&copy; Copyright 2017 - ${new Date().getFullYear().toString()} Agesoft. All Rights Reserved.</p><br>
+`
+
+document.addEventListener('DOMContentLoaded', e => {
+    Footer.innerHTML = FooterHTML;
+    Header.innerHTML = HeaderHTML;
+})
